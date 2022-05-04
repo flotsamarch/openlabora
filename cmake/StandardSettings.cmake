d9e5e2eb9a76736a1818786db4d581aced2d1a21
user <email>
2022-05-04 18:18:06 +0400

    Build configuration

--- cmake/StandardSettings.cmake
+++ cmake/StandardSettings.cmake
@@ -14,15 +14,12 @@ option(${PROJECT_NAME}_WARNINGS_AS_ERRORS "Treat compiler warnings as errors." O
 #
 # Package managers
 #
-# Currently supporting: Conan, Vcpkg.
 
-#option(${PROJECT_NAME}_ENABLE_CONAN "Enable the Conan package manager for this project." OFF)
 option(${PROJECT_NAME}_ENABLE_VCPKG "Enable the Vcpkg package manager for this project." ON)
 
 #
 # Unit testing
 #
-# Currently supporting: GoogleTest, Catch2.
 
 option(${PROJECT_NAME}_ENABLE_UNIT_TESTING "Enable unit tests for the projects (from the `test` subfolder)." ON)
 
