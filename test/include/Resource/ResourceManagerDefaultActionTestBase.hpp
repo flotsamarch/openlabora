d181176f1f590fd40bdac9569c2cb12b1a651813
flotsamarch <flotsamarch@users.noreply.github.com>
2022-11-07 16:42:53 +0300

    License file

--- test/include/Resource/ResourceManagerDefaultActionTestBase.hpp
+++ test/include/Resource/ResourceManagerDefaultActionTestBase.hpp
@@ -1,3 +1,15 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//
+// OpenLabora (c) by Flotsamarch (https://github.com/flotsamarch)
+//
+// This work is licensed under the Creative Commons
+// Attribution-NonCommercial-ShareAlike 4.0 International License.
+//
+// You should have received a copy of the license along with this
+// work. If not, see <http://creativecommons.org/licenses/by-nc-sa/4.0/>.
+//
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
 #ifndef RESOURCEMANAGERDEFAULTACTIONTESTBASE_HPP_
 #define RESOURCEMANAGERDEFAULTACTIONTESTBASE_HPP_
 
